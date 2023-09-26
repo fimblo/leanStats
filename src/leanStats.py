8923e63f962fd39e70915f99939edade3fa92657
Mattias Jansson <fimblo@yanson.org>
2023-09-26 22:06:58 +0200

    style: apply black style formatting

--- src/leanStats.py
+++ src/leanStats.py
@@ -4,64 +4,76 @@ import argparse
 import configparser
 import pandas as pd
 import numpy as np
-#from pprint import pprint
+
+# from pprint import pprint
 import sys
 import os
 
-def extract_ticket_timestamps(file_path):
-    data = pd.read_csv(file_path,
-                       parse_dates=['Status Transition.date'],
-                       dayfirst=True) # dd/mm/yy madness
-    
-    # filter when tickets moved to IN PROGRESS
-    in_progress = (data[data['Status Transition.to'].str.upper() == 'IN PROGRESS']
-                   .groupby('Key')
-                   .agg({'Status Transition.date': 'min'})
-                   .rename(columns={'Status Transition.date': 'timestamp_start'}))
-    
-    # filter when tickets moved to DONE
-    done = (data[data['Status Transition.to'].str.upper() == 'DONE']
-            .groupby('Key')
-            .agg({'Status Transition.date': 'max'})
-            .rename(columns={'Status Transition.date': 'timestamp_end'}))
-    
-    # left join on key.
-    return in_progress.join(done, on='Key').reset_index()
 
+def extract_ticket_timestamps(file_path):
+    data = pd.read_csv(
+        file_path, parse_dates=["Status Transition.date"], dayfirst=True
+    )  # dd/mm/yy madness
+
+    # filter when tickets moved to IN PROGRESS
+    in_progress = (
+        data[data["Status Transition.to"].str.upper() == "IN PROGRESS"]
+        .groupby("Key")
+        .agg({"Status Transition.date": "min"})
+        .rename(columns={"Status Transition.date": "timestamp_start"})
+    )
+
+    # filter when tickets moved to DONE
+    done = (
+        data[data["Status Transition.to"].str.upper() == "DONE"]
+        .groupby("Key")
+        .agg({"Status Transition.date": "max"})
+        .rename(columns={"Status Transition.date": "timestamp_end"})
+    )
+
+    # left join on key.
+    return in_progress.join(done, on="Key").reset_index()
 
 
 def calculate_cycletime(dataframe):
     df_copy = dataframe.copy()
-    
-    time_difference      = df_copy['timestamp_end'] - df_copy['timestamp_start']
-    total_seconds        = time_difference.dt.total_seconds()
-    cycletime_in_days    = np.ceil(total_seconds / (24 * 3600))
-    df_copy['cycletime'] = cycletime_in_days.astype(int)
+
+    time_difference = df_copy["timestamp_end"] - df_copy["timestamp_start"]
+    total_seconds = time_difference.dt.total_seconds()
+    cycletime_in_days = np.ceil(total_seconds / (24 * 3600))
+    df_copy["cycletime"] = cycletime_in_days.astype(int)
 
     return df_copy
 
 
 def compute_metrics(dataframe, lookback_window):
     # Sort dataframe by 'timestamp_end'
-    dataframe = dataframe.sort_values(by='timestamp_end')
+    dataframe = dataframe.sort_values(by="timestamp_end")
 
     # Initialize new columns
-    dataframe['median_cycletime'] = np.nan
-    dataframe['p85_cycletime'] = np.nan
-    dataframe['throughput'] = np.nan
+    dataframe["median_cycletime"] = np.nan
+    dataframe["p85_cycletime"] = np.nan
+    dataframe["throughput"] = np.nan
 
     # For each ticket, compute metrics over a 7-day lookback window
     for idx, row in dataframe.iterrows():
-        lookback_start = row['timestamp_end'] - pd.Timedelta(days = lookback_window)
-        lookback_end = row['timestamp_end']
+        lookback_start = row["timestamp_end"] - pd.Timedelta(days=lookback_window)
+        lookback_end = row["timestamp_end"]
 
         # Filter dataframe for tickets within the lookback window
-        lookback_data = dataframe[(dataframe['timestamp_end'] >= lookback_start) & (dataframe['timestamp_end'] <= lookback_end)]
+        lookback_data = dataframe[
+            (dataframe["timestamp_end"] >= lookback_start)
+            & (dataframe["timestamp_end"] <= lookback_end)
+        ]
 
         # Compute metrics
-        dataframe.at[idx, 'median_cycletime'] = np.ceil(lookback_data['cycletime'].median())
-        dataframe.at[idx, 'p85_cycletime']    = np.ceil(lookback_data['cycletime'].quantile(0.85))
-        dataframe.at[idx, 'throughput']       = np.ceil(lookback_data.shape[0])
+        dataframe.at[idx, "median_cycletime"] = np.ceil(
+            lookback_data["cycletime"].median()
+        )
+        dataframe.at[idx, "p85_cycletime"] = np.ceil(
+            lookback_data["cycletime"].quantile(0.85)
+        )
+        dataframe.at[idx, "throughput"] = np.ceil(lookback_data.shape[0])
 
     return dataframe
 
@@ -72,17 +84,30 @@ def print_help():
 
 def main():
     parser = argparse.ArgumentParser(description="calculate lean metrics")
-    parser.add_argument('-c', '--config-file',
-                        help='Path to config file. Overrides all other commandline params if specified.',
-                        type=str, required=False)
-    parser.add_argument('-i', '--input-csv-file',
-                        help='Path to input file with CSV values.',
-                        type=str, required=False)
-    parser.add_argument('-w', '--lookback-window',
-                        help='Size of window in days for metric calculations. (default 7)',
-                        type=int, default=7, required=False)
+    parser.add_argument(
+        "-c",
+        "--config-file",
+        help="Path to config file. Overrides all other commandline params if specified.",
+        type=str,
+        required=False,
+    )
+    parser.add_argument(
+        "-i",
+        "--input-csv-file",
+        help="Path to input file with CSV values.",
+        type=str,
+        required=False,
+    )
+    parser.add_argument(
+        "-w",
+        "--lookback-window",
+        help="Size of window in days for metric calculations. (default 7)",
+        type=int,
+        default=7,
+        required=False,
+    )
     args = parser.parse_args()
-    
+
     # If a configuration file is provided, overwrite all command-line
     # arguments
     if args.config_file:
@@ -102,41 +127,52 @@ def main():
         file_path = args.input_csv_file
         lookback_window = args.lookback_window
 
-
     # Validate command line arguments
     if not args.config_file and not file_path:
         print("Error: Either -c (config file) or -i (input file) must be provided.")
         sys.exit(1)
-
-
-
 
     # Sanity checks
     if not os.path.isfile(file_path):
         print(f"The file '{file_path}' does not exist or is not readable.")
         sys.exit(1)
 
-
     # read in data and calculate cycletime
     dataframe = extract_ticket_timestamps(file_path)
     dataframe = calculate_cycletime(dataframe)
     dataframe = compute_metrics(dataframe, lookback_window)
-    
-    # select which fields I want
-    dataframe = dataframe[['Key', 'timestamp_start', 'timestamp_end', 'cycletime', 'median_cycletime', 'p85_cycletime', 'throughput']]
 
+    # select which fields I want
+    dataframe = dataframe[
+        [
+            "Key",
+            "timestamp_start",
+            "timestamp_end",
+            "cycletime",
+            "median_cycletime",
+            "p85_cycletime",
+            "throughput",
+        ]
+    ]
 
     # Print to stdout
     def format_value(value):
         s = str(value)
-        return s.rstrip('.0') if '.' in s else s
-    tickets_data = dataframe.sort_values(by='timestamp_end').values.tolist()
-    headers = ['Key', 'timestamp_start', 'timestamp_end', 'cycletime', 'median_cycletime', 'p85_cycletime', 'throughput']
+        return s.rstrip(".0") if "." in s else s
+
+    tickets_data = dataframe.sort_values(by="timestamp_end").values.tolist()
+    headers = [
+        "Key",
+        "timestamp_start",
+        "timestamp_end",
+        "cycletime",
+        "median_cycletime",
+        "p85_cycletime",
+        "throughput",
+    ]
     print("|".join(headers))
     for row in tickets_data:
         print("|".join(map(format_value, row)))
-
-
 
 
 if __name__ == "__main__":
--- tests/test_leanStats.py
+++ tests/test_leanStats.py
@@ -5,6 +5,7 @@ import sys
 import os
 
 from leanStats import extract_ticket_timestamps, calculate_cycletime, compute_metrics
+
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -17,6 +18,8 @@ TICKET-2,16/09/2023 00:01:00,IN PROGRESS
 TICKET-2,20/09/2023 00:02:00,DONE
 """
     return StringIO(data)
+
+
 @pytest.fixture
 def sample_data_date():
     data = """Key,Status Transition.date,Status Transition.to
@@ -27,79 +30,94 @@ TICKET-2,20/09/2023,DONE
 """
     return StringIO(data)
 
+
 def test_extract_ticket_datestamps(sample_data_date):
     df = extract_ticket_timestamps(sample_data_date)
 
     assert len(df) == 2
-    assert df.iloc[0]['Key'] == 'TICKET-1'
-    assert df.iloc[0]['timestamp_start'].strftime('%Y-%m-%d') == '2023-09-15'
-    assert df.iloc[0]['timestamp_end'].strftime('%Y-%m-%d') == '2023-09-17'
-    assert df.iloc[1]['Key'] == 'TICKET-2'
-    assert df.iloc[1]['timestamp_start'].strftime('%Y-%m-%d') == '2023-09-16'
-    assert df.iloc[1]['timestamp_end'].strftime('%Y-%m-%d') == '2023-09-20'
-
+    assert df.iloc[0]["Key"] == "TICKET-1"
+    assert df.iloc[0]["timestamp_start"].strftime("%Y-%m-%d") == "2023-09-15"
+    assert df.iloc[0]["timestamp_end"].strftime("%Y-%m-%d") == "2023-09-17"
+    assert df.iloc[1]["Key"] == "TICKET-2"
+    assert df.iloc[1]["timestamp_start"].strftime("%Y-%m-%d") == "2023-09-16"
+    assert df.iloc[1]["timestamp_end"].strftime("%Y-%m-%d") == "2023-09-20"
 
 
 def test_extract_ticket_timestamps(sample_data_time):
     df = extract_ticket_timestamps(sample_data_time)
 
     assert len(df) == 2
-    assert df.iloc[0]['Key'] == 'TICKET-1'
-    assert df.iloc[0]['timestamp_start'].strftime('%Y-%m-%d %H:%M:%S') == '2023-09-15 00:01:00'
-    assert df.iloc[0]['timestamp_end'].strftime('%Y-%m-%d %H:%M:%S') == '2023-09-17 00:02:00'
-    assert df.iloc[1]['Key'] == 'TICKET-2'
-    assert df.iloc[1]['timestamp_start'].strftime('%Y-%m-%d %H:%M:%S') == '2023-09-16 00:01:00'
-    assert df.iloc[1]['timestamp_end'].strftime('%Y-%m-%d %H:%M:%S') == '2023-09-20 00:02:00'
-
+    assert df.iloc[0]["Key"] == "TICKET-1"
+    assert (
+        df.iloc[0]["timestamp_start"].strftime("%Y-%m-%d %H:%M:%S")
+        == "2023-09-15 00:01:00"
+    )
+    assert (
+        df.iloc[0]["timestamp_end"].strftime("%Y-%m-%d %H:%M:%S")
+        == "2023-09-17 00:02:00"
+    )
+    assert df.iloc[1]["Key"] == "TICKET-2"
+    assert (
+        df.iloc[1]["timestamp_start"].strftime("%Y-%m-%d %H:%M:%S")
+        == "2023-09-16 00:01:00"
+    )
+    assert (
+        df.iloc[1]["timestamp_end"].strftime("%Y-%m-%d %H:%M:%S")
+        == "2023-09-20 00:02:00"
+    )
 
 
 def test_calculate_cycletime(sample_data_time):
     df = extract_ticket_timestamps(sample_data_time)
     df = calculate_cycletime(df)
 
-    # we always round cycle time up. 
+    # we always round cycle time up.
     assert len(df) == 2
-    assert df.iloc[0]['cycletime'] == 3  # 3 days from '2023-09-15' to '2023-09-17 + 1 minute'
-    assert df.iloc[1]['cycletime'] == 5  # 5 days from '2023-09-16' to '2023-09-20 + 1 minute'
-
+    assert (
+        df.iloc[0]["cycletime"] == 3
+    )  # 3 days from '2023-09-15' to '2023-09-17 + 1 minute'
+    assert (
+        df.iloc[1]["cycletime"] == 5
+    )  # 5 days from '2023-09-16' to '2023-09-20 + 1 minute'
 
 
 def test_compute_metrics_basic_functionality():
     # Given: Create a mock dataframe
     data = {
-        'Key': ['A', 'B', 'C', 'D'],
-        'timestamp_end': [pd.Timestamp('2023-09-15 10:00:00'),
-                          pd.Timestamp('2023-09-17 10:00:00'),
-                          pd.Timestamp('2023-09-19 10:00:00'),
-                          pd.Timestamp('2023-09-20 10:00:00')],
-        'cycletime': [5, 3, 4, 2]
+        "Key": ["A", "B", "C", "D"],
+        "timestamp_end": [
+            pd.Timestamp("2023-09-15 10:00:00"),
+            pd.Timestamp("2023-09-17 10:00:00"),
+            pd.Timestamp("2023-09-19 10:00:00"),
+            pd.Timestamp("2023-09-20 10:00:00"),
+        ],
+        "cycletime": [5, 3, 4, 2],
     }
     df = pd.DataFrame(data)
-    
+
     # When: Run the compute_metrics function with 7 days lookback
     result_df = compute_metrics(df, 7)
-    
+
     # Then: Check the basic functionality
 
     # Check columns are present
-    assert 'median_cycletime' in result_df.columns
-    assert 'p85_cycletime' in result_df.columns
-    assert 'throughput' in result_df.columns
+    assert "median_cycletime" in result_df.columns
+    assert "p85_cycletime" in result_df.columns
+    assert "throughput" in result_df.columns
 
     # Check correct calculations for each ticket
-    assert result_df.loc[result_df['Key'] == 'A', 'median_cycletime'].values[0] == 5
-    assert result_df.loc[result_df['Key'] == 'A', 'p85_cycletime'].values[0] == 5
-    assert result_df.loc[result_df['Key'] == 'A', 'throughput'].values[0] == 1
+    assert result_df.loc[result_df["Key"] == "A", "median_cycletime"].values[0] == 5
+    assert result_df.loc[result_df["Key"] == "A", "p85_cycletime"].values[0] == 5
+    assert result_df.loc[result_df["Key"] == "A", "throughput"].values[0] == 1
 
-    assert result_df.loc[result_df['Key'] == 'B', 'median_cycletime'].values[0] == 4
-    assert result_df.loc[result_df['Key'] == 'B', 'p85_cycletime'].values[0] == 5
-    assert result_df.loc[result_df['Key'] == 'B', 'throughput'].values[0] == 2
+    assert result_df.loc[result_df["Key"] == "B", "median_cycletime"].values[0] == 4
+    assert result_df.loc[result_df["Key"] == "B", "p85_cycletime"].values[0] == 5
+    assert result_df.loc[result_df["Key"] == "B", "throughput"].values[0] == 2
 
-    assert result_df.loc[result_df['Key'] == 'C', 'median_cycletime'].values[0] == 4
-    assert result_df.loc[result_df['Key'] == 'C', 'p85_cycletime'].values[0] == 5
-    assert result_df.loc[result_df['Key'] == 'C', 'throughput'].values[0] == 3
+    assert result_df.loc[result_df["Key"] == "C", "median_cycletime"].values[0] == 4
+    assert result_df.loc[result_df["Key"] == "C", "p85_cycletime"].values[0] == 5
+    assert result_df.loc[result_df["Key"] == "C", "throughput"].values[0] == 3
 
-    assert result_df.loc[result_df['Key'] == 'D', 'median_cycletime'].values[0] == 4
-    assert result_df.loc[result_df['Key'] == 'D', 'p85_cycletime'].values[0] == 5
-    assert result_df.loc[result_df['Key'] == 'D', 'throughput'].values[0] == 4
-
+    assert result_df.loc[result_df["Key"] == "D", "median_cycletime"].values[0] == 4
+    assert result_df.loc[result_df["Key"] == "D", "p85_cycletime"].values[0] == 5
+    assert result_df.loc[result_df["Key"] == "D", "throughput"].values[0] == 4
